039a19d174177c07ef9639142fa1c6b39f65557a
Chen Eini <chen.eini000@gmail.com>
2020-11-24 23:46:29 +0200

    add generate cards functionality

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,16 @@
+const NUMBER_OF_CARDS_IN_GAME = 20;
+const THEME_COLOR = "#ff0083";
+
+const CARD_WIDTH = 98;
+const CARD_HEIGHT = 135;
+
+export function generateCards() {
+  return Array.from({ length: NUMBER_OF_CARDS_IN_GAME }, (_, i) => i + 1);
+}
+
 export function shuffle(array) {
   return array
     .map((a) => ({ sort: Math.random(), value: a }))
     .sort((a, b) => a.sort - b.sort)
     .map((a) => a.value);
 }
-
-const NUMBER_OF_CARDS_IN_GAME = 20;
-
-const THEME_COLOR = "#ff0083";
-
-const CARD_WIDTH = 98;
-const CARD_HEIGHT = 135;
